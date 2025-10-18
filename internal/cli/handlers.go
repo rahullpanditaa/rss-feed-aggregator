573c8534357445093a827b57dab09c643fb10a35
Rahul Pandita <rpandita1999@outlook.com>
2025-10-18 07:22:41 +0000

    users command works

--- internal/cli/handlers.go
+++ internal/cli/handlers.go
@@ -109,6 +109,10 @@ func HandlerUsers(s *State, cmd Command) error {
 		fmt.Fprintln(os.Stderr, "could not retrieve all users:", err)
 		os.Exit(1)
 	}
+	if len(allUsers) == 0 {
+		fmt.Println("no users found")
+		return nil
+	}
 	for _, user := range allUsers {
 		if user.Name == s.ApplicationState.CurrentUserName {
 			fmt.Printf("* %s (current)\n", user.Name)
