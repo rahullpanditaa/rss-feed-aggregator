bc0bd0483ce0492077bebac7192e741a59ddf61c
Rahul Pandita <rpandita1999@outlook.com>
2025-10-19 02:48:54 +0000

    create function to fetch rss feed, unmarshal xml and return corresponding struct

---
+++ internal/rss/fetch_feed.go
@@ -0,0 +1,48 @@
+package rss
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+)
+
+func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	// create a pointer to a new http GET request
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "feed_aggregator")
+
+	// create http client
+	client := &http.Client{}
+
+	// do the GET request, get a response
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(body, &rssFeed)
+	if err != nil {
+		return nil, err
+	}
+
+	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
+	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
+	for i := range rssFeed.Channel.Item {
+		rssFeed.Channel.Item[i].Title = html.UnescapeString(rssFeed.Channel.Item[i].Title)
+		rssFeed.Channel.Item[i].Description = html.UnescapeString(rssFeed.Channel.Item[i].Description)
+	}
+	return &rssFeed, nil
+
+}
