bffb7b64822387d0c7f9873f9680b37d43c4545c
Rahul Pandita <rpandita1999@outlook.com>
2025-10-19 05:40:39 +0000

    create query to create a feed follow record; return its fields plus linked user and feed; sqlc generate

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,15 @@
+-- name: CreateFeedFollow :one
+WITH "inserted_feed_follow" AS (
+    INSERT INTO "feed_follows" (
+        "id", "created_at", "updated_at", "user_id", "feed_id"
+    ) VALUES (
+        $1, $2, $3, $4, $5
+    )
+    RETURNING *
+)
+SELECT "inserted_feed_follow".*, "users"."name", "feeds"."name"
+FROM "inserted_feed_follow"
+JOIN "users" 
+ON "inserted_feed_follow"."user_id" = "users"."id"
+JOIN "feeds"
+ON "inserted_feed_follow"."feed_id" = "feeds"."id";
