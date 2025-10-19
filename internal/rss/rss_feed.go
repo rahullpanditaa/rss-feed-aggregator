8d7e95bf946c4fd5143dde0ca84c8e21ef5a5502
Rahul Pandita <rpandita1999@outlook.com>
2025-10-19 02:19:37 +0000

    create structs for unmarshalling rss feed xml

---
+++ internal/rss/rss_feed.go
@@ -0,0 +1,10 @@
+package rss
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
---
+++ internal/rss/rss_item.go
@@ -0,0 +1,8 @@
+package rss
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
