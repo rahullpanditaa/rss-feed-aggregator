cf1335032fbc6a1c9461797c72c9ccf76a5c8168
Rahul Pandita <rpandita1999@outlook.com>
2025-10-20 04:52:16 +0000

    create queries for create post and get posts for user; sqlc generate

---
+++ sql/queries/posts.sql
@@ -0,0 +1,20 @@
+-- name: CreatePost :exec
+INSERT INTO "posts" (
+    "id", "created_at", "updated_at", "title", "url",
+    "description", "published_at", "feed_id"
+)
+VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8
+);
+
+-- name: GetPostsForUser :many
+SELECT * 
+FROM "posts"
+JOIN "feed_follows"
+ON "posts"."feed_id" = "feed_follows"."feed_id"
+WHERE "feed_dollows"."user_id" = (
+    SELECT "id" FROM "users"
+    WHERE "name" = $1
+)
+ORDER BY "published_at" DESC 
+LIMIT $2;
