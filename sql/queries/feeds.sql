b50b832f772796fc10180c15e6a8b15554034d7f
Rahul Pandita <rpandita1999@outlook.com>
2025-10-20 02:19:02 +0000

    create a query to set last_fetched_at and updated_at in feeds to current time

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -11,3 +11,10 @@ RETURNING *;
 -- name: GetFeedsByURL :one
 SELECT * FROM "feeds"
 WHERE "url"=$1;
+
+-- name: MarkFeedFetched :exec
+UPDATE "feeds"
+SET 
+"updated_at" = NOW(),
+"last_fetched_at" = NOW()
+WHERE "id" = $1;
