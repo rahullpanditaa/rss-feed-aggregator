54f8127d54881e3c04de8dbb17ae26aa0f55ca13
Rahul Pandita <rpandita1999@outlook.com>
2025-10-20 04:41:05 +0000

    migrations for posts table

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -5,12 +5,12 @@ CREATE TABLE "posts" (
     "updated_at" TIMESTAMP NOT NULL,
     "title" TEXT NOT NULL,
     "url" TEXT NOT NULL UNIQUE,
-    "description" TEXT NOT NULL,
-    "published_at" TIMESTAMP NOT NULL,
+    "description" TEXT,
+    "published_at" TIMESTAMP,
     "feed_id" UUID NOT NULL,
     PRIMARY KEY("id"),
     FOREIGN KEY("feed_id") REFERENCES "feeds"("id") ON DELETE CASCADE
-)
+);
 
 -- +goose Down
 DROP TABLE "posts";
